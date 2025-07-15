b06c2a9bb5899c953bfcb2eaa03df7cc6e9291c0
saulo.rodrigues <saulohrodrigues@gmail.com>
2025-07-15 14:37:40 -0300

    Subindo a estrutura inicial do projeto

---
+++ src/main/java/com/animaisparaadocao/animaisparaadocao/AnimaisparaadocaoApplication.java
@@ -0,0 +1,13 @@
+package com.animaisparaadocao.animaisparaadocao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimaisparaadocaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimaisparaadocaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/animaisparaadocao/animaisparaadocao/AnimaisparaadocaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.animaisparaadocao.animaisparaadocao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimaisparaadocaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
