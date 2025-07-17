1399f52b1f2deef6db14e44f7ca1a0295b40cf3f
saulo.rodrigues <saulohrodrigues@gmail.com>
2025-07-17 14:02:29 -0300

    Criando a estrutura de exceções personalisadas

---
+++ src/main/java/com/animaisparaadocao/animaisparaadocao/exception/especies/AnimalJaCadastradoException.java
@@ -0,0 +1,7 @@
+package com.animaisparaadocao.animaisparaadocao.exception.especies;
+
+public class AnimalJaCadastradoException extends RuntimeException{
+    public AnimalJaCadastradoException(String mensagem){
+        super(mensagem);
+    }
+}
